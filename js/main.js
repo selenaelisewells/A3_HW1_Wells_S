daf2423aa1390e6f13de8e290b9050074e3acd5d
Selena Wells <selena.elise.wells@gmail.com>
2020-10-15 22:25:51 -0400

    dynamic content added to html

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@
     fetch('./data/classData.json')
         .then(res => res.json())
         .then(data => {
-            debugger;
-            console.log(data);
-
+            // debugger;
+            console.log(data.profname);
+            setContent(data);
             //handleData
             //here's where you would call the function that puts the pieces on the page
         })
@@ -14,5 +14,31 @@
     .catch((err) => {
         console.log(err);
     })
+
+    function setContent(data) {
+        //set course name
+        let coursename = document.querySelector('.course-name');
+        coursename.innerHTML = data.coursename;
+
+        let coursecode = document.querySelector('.course-code')
+        coursecode.innerHTML = data.coursecode;
+
+        //set prof name
+        let profName = document.querySelector('.prof-name')
+        profName.innerHTML = `Professor- ${data.profname}`;
+
+
+
+
+        //set class times
+        let classTimeList = document.querySelector(".list-inline");
+        let content = '';
+
+        data.classtime.forEach(time => {
+            content += `<li>${time}</li>`;
+        })
+        classTimeList.innerHTML = content;
+        //set 
+    }
 
 })();
